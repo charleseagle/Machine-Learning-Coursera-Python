ff7ab8f22275b646fb0992ee27898bc58c005721
charleseagle <sunqicharles@gmail.com>
2015-10-23 11:30:21 -0600

    Revert "Week5"
    This reverts commit 17a0a4e8052474916e61c7fa36388700d737cba1.

--- Week5/DebugInitializeWeights.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division
-import numpy as np
-
-
-def DebugInitializeWeights(fan_out, fan_in):
-    W = np.zeros((fan_out, 1 + fan_in))
-    W = np.sin(np.arange(len(W.flatten()))+1).reshape(np.shape(W))/10
-    
-    return W
--- Week5/SigmoidGradient.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division
-import numpy as np
-
-
-def SigmoidGradient(z):
-    g = np.zeros(np.shape(z))
-    g = 1/(1+np.exp(-z))*(1-1/(1+np.exp(-z)))
-    return g
-    
